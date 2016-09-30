1e1c629371e60ce2394525a26750a2c94ee8fea4
Diego <Dialpe22@gmail.com>
2016-09-30 12:39:29 +0200

    Add files via upload

---
+++ Boletín22.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletín.pkg2.pkg2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dalonsoperez
+ */
+public class Boletín22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Para calcular el area de un cuadrado:");
+        
+        double lado;
+       
+        lado = Double.parseDouble(JOptionPane.showInputDialog("Introduce el lado: "));
+        
+        System.out.println("El area del cuadrado de lado " + lado + "cm es igual a " + lado * lado + "cm.");
+
+    }
+    
+}
