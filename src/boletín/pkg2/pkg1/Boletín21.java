b7bed003e1b644ec8dd54c8796c3c002d94419da
dalonsoperez <dalonsoperez@atanasoff23.danielcastelao.org>
2016-09-30 12:29:53 +0200

    Ejercicio 1 del boletín 2

---
+++ src/boletín/pkg2/pkg1/Boletín21.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletín.pkg2.pkg1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dalonsoperez
+ */
+public class Boletín21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Para calcular el area de un triánguno:");
+        
+        double base;
+        double altura;
+        
+        base = Double.parseDouble(JOptionPane.showInputDialog("Introduce base: "));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Introduce altura: "));
+       
+        System.out.println("El area del triángulo de base " + base + "cm y altura " + altura + "cm es igual a " + base * altura/2 + "cm.");
+               
+             
+    }
+
+}
